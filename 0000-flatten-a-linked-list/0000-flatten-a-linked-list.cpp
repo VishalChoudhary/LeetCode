7a9cd181a02aa32a6bd716820ed3edd65f55ef0b
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-08-22 19:30:40 +0530

    Time: 23 ms (53.05%), Space: 77.6 MB (29.35%) - LeetHub

---
+++ 0000-flatten-a-linked-list/0000-flatten-a-linked-list.cpp
@@ -0,0 +1,43 @@
+ * Definition for linked list.
+ * class Node {
+ *  public:
+ *		int data;
+ *		Node *next;
+ * 		Node *child;
+ *		Node() : data(0), next(nullptr), child(nullptr){};
+ *		Node(int x) : data(x), next(nullptr), child(nullptr) {}
+ *		Node(int x, Node *next, Node *child) : data(x), next(next), child(child) {}
+ * };
+ */
+
+Node *merge(Node *list1,Node *list2){
+	Node *dummyNode=new Node(-1);
+	Node *res=dummyNode;
+	while(list1 != NULL && list2!=NULL){
+		if(list1->data <list2->data){
+			res->child=list1;
+			res=list1;
+			list1=list1->child;
+		}
+		else{
+			res->child=list2;
+			res=list2;
+			list2=list2->child;
+		}
+		res->next=NULL;
+	}
+	if(list1) res->child=list1;
+	else res->child=list2;
+	if(dummyNode->child) dummyNode->child->next=NULL;
+	return dummyNode->child;
+}
+
+Node* flattenLinkedList(Node* head) 
+{
+	// Write your code here
+	if(head==NULL || head->next==NULL)
+	return head;
+	Node *mergehead=flattenLinkedList(head->next);
+	head=merge(head, mergehead);
+	return head;
+}
