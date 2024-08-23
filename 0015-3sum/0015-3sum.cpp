26e1904ced38cc44a119eacbdfcabbe3ee33369b
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-08-23 20:10:11 +0530

    Time: 1767 ms (5.05%), Space: 183.8 MB (20.44%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        //Two Pointer Technique 
+        vector<vector<int>> ans;
+        set<vector<int>> t;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n-2;i++){
+            int l=i+1,h=n-1;
+            while(l<h){
+                int current=nums[i]+nums[l]+nums[h];
+                if(current==0){
+                    t.insert({nums[i],nums[l],nums[h]});
+                    l++;
+                    h--;
+                }
+                else if(current>0)
+                    h--;
+                else
+                    l++;
+            }
+        }
+        for(auto x:t){
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
