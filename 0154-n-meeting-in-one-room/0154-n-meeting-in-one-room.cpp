9fcb4432f3b595dc4aa82d8734b0b63d655a2afa
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-08-24 21:46:12 +0530

    Time: 3 ms (95.37%), Space: 11.2 MB (70.12%) - LeetHub

--- 0154-n-meeting-in-one-room/0154-n-meeting-in-one-room.cpp
+++ 0154-n-meeting-in-one-room/0154-n-meeting-in-one-room.cpp
@@ -1,8 +1,31 @@
 class Solution {
-    
+    struct meeting{
+        int s;
+        int e;
+        int pos;
+    };
 public:
+    bool static comparator(struct meeting m1,meeting m2){
+        if(m1.e<m2.e) return true;
+        return false;
+    }
     // Function to find the maximum number of meetings that can
     // be performed in a meeting room.
     int maxMeetings(int n, int start[], int end[]) {
+        // Your code here
+        struct meeting meet[n];
+        for(int i=0;i<n;i++){
+            meet[i].s=start[i],meet[i].e=end[i],meet[i].pos=i+1;
+        }
+        sort(meet,meet+n,comparator);
+        int freetime=meet[0].e;
+        int cnt=1;
+        for(int i=1;i<n;i++){
+            if(meet[i].s>freetime){
+                cnt++;
+                freetime=meet[i].e;
+            }
+        }
+        return cnt;
     }
 };
