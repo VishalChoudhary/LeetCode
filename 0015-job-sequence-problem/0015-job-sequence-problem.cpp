9ecea98b291e776f94b77802c4cbec6e4710e124
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-08-25 01:49:45 +0530

    Time: 10 ms (38.1%), Space: 21.4 MB (46.46%) - LeetHub

--- 0015-job-sequence-problem/0015-job-sequence-problem.cpp
+++ 0015-job-sequence-problem/0015-job-sequence-problem.cpp
@@ -1,4 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+// A structure to represent a job 
+struct Job 
+{ 
+    int id;	 // Job Id 
+    int dead; // Deadline of job 
+    int profit; // Profit if job is over before or on deadline 
+}; 
+
 class Solution 
 {
-    
+    public:
+    bool static comparison(Job j1,Job j2){
+        return(j1.profit>j2.profit);
+    }
+    //Function to find the maximum profit and the number of jobs done.
+    vector<int> JobScheduling(Job arr[], int n) 
+    { 
+        // your code here
+        sort(arr,arr+n,comparison);
+        int maxdead=arr[0].dead;
+        for(int i=1;i<n;i++){
+            maxdead=max(maxdead,arr[i].dead);
+        }
+        int slot[maxdead+1];
+        for(int i=0;i<=maxdead;i++){
+            slot[i]=-1;
+        }
+        int cntJob=0,jobProfit=0;
+        for(int i=0;i<n;i++){
+            for(int j=arr[i].dead;j>0;j--){
+                if(slot[j]==-1){
+                    slot[j]=arr[i].id;
+                    cntJob++;
+                    jobProfit+=arr[i].profit;
+                    break;
+                }
+            }
+        }
+        return {cntJob,jobProfit};
+    } 
 };
+
+//{ Driver Code Starts.
+// Driver program to test methods 
+int main() 
+{ 
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        Job arr[n];
+        
+        //adding id, deadline, profit
+        for(int i = 0;i<n;i++){
+                int x, y, z;
+                cin >> x >> y >> z;
+                arr[i].id = x;
+                arr[i].dead = y;
+                arr[i].profit = z;
+        }
+        Solution ob;
+        //function call
+        vector<int> ans = ob.JobScheduling(arr, n);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+	return 0; 
+}
+
