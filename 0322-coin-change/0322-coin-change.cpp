ceb2f35c9b019785bfb2cd9379be5d067794ee38
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-08-25 16:20:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    int solve(vector<int> &coins,int x,vector<int> &dp){
-        if(x==0)
-            return 0;
-        if(x<0)
-            return INT_MAX;
-        
-        if(dp[x]!=-1)
-            return dp[x];
-        int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            int ans=solve(coins,x-coins[i],dp);
-            if(ans!=INT_MAX)
-                mini=min(mini,1+ans);
-        }
-        dp[x]=mini;
-        return dp[x];
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<int> dp(amount+1,-1);
-        int res=solve(coins,amount,dp);
-        if(res==INT_MAX)
-            return -1;
+        if(amount==0)
+        return 0;
+        int res=0;
+        vector<int> ans;
+        for(int i=n-1;i>=0;i--){
+            while(amount>=coins[i]){
+                amount-=coins[i];
+                res++;
+                ans.push_back(coins[i]);
+            }
+        }
+        if(ans.size()==1)
+        return -1;
+        else
         return res;
     }
 };
