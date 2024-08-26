5b3ccbc70f3d36e1a8edf55789c77aa250fc9b1e
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-08-26 16:53:18 +0530

    Time: 4 ms (63.64%), Space: 19.7 MB (38.5%) - LeetHub

--- 0019-subset-sum/0019-subset-sum.cpp
+++ 0019-subset-sum/0019-subset-sum.cpp
@@ -1,4 +1,18 @@
 class Solution {
   public:
-    
+    void func(int ind,int sum,vector<int> &arr,int N,vector<int> &subSetSum){
+        if(ind==N){
+            subSetSum.push_back(sum);
+            return;
+        }
+        func(ind+1,sum+arr[ind],arr,N,subSetSum);
+        func(ind+1,sum,arr,N,subSetSum);
+    }
+    vector<int> subsetSums(vector<int> arr, int n) {
+        // Write Your Code here
+        vector<int>subSetSum;   
+        func(0,0,arr,n,subSetSum);
+        sort(subSetSum.begin(),subSetSum.end());
+        return subSetSum;
+    }
 };
