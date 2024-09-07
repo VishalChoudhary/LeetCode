c669743cd763a15e7562dc34d6d5b463fb8a0330
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-07 19:50:03 +0530

    Time: 3 ms (69.52%), Space: 11.4 MB (29.47%) - LeetHub

--- 0171-boundary-of-binary-tree/0171-boundary-of-binary-tree.cpp
+++ 0171-boundary-of-binary-tree/0171-boundary-of-binary-tree.cpp
@@ -1,30 +1,51 @@
-void addLeftBoundary(TreeNode *root,vector<int> &res){
-    TreeNode *curr=root->left;
-    while(curr){
+class Solution {
+public:
+    bool isLeaf(Node* root) {
+        return !root->left && !root->right;
+    }
+    
+    void addLeftBoundary(Node *root,vector<int> &res){
+        Node *curr=root->left;
+        while(curr){
         if(!isLeaf(curr)) res.push_back(curr->data);
         if(curr->left) curr=curr->left;
         else curr=curr->right;
+        }
     }
-}
 
-void addRightBoundary(TreeNode *root,vector<int> &res){
-    TreeNode *curr=root->right;
-    vector<int> temp;
-    while(curr){
-        if(isLeaf(curr)) temp.push_back(curr->data);
-        if(curr->right) curr=curr->right;
-        else curr=curr->left;
+    void addRightBoundary(Node *root,vector<int> &res){
+        Node *curr=root->right;
+        vector<int> temp;
+        while(curr){
+            if(!isLeaf(curr)) temp.push_back(curr->data);
+            if(curr->right) curr=curr->right;
+            else curr=curr->left;
+        }
+        for(int i=temp.size()-1;i>=0;i--){
+            res.push_back(temp[i]);
+        }
     }
-    for(int i=temp.size()-1;i>=0;i--){
-        res.push_back(temp[i]);
-    }
-}
 
-void addLeaves(TreeNode *root,vector<int> &res){
-    if(isLeaf(root)){
+    void addLeaves(Node *root,vector<int> &res){
+        if(isLeaf(root)){
+            res.push_back(root->data);
+            return;
+        }
+        if(root->left) addLeaves(root->left, res);
+        if(root->right) addLeaves(root->right, res);
+    }
+
+    vector <int> boundary(Node *root)
+    {
+        //Your code here
+        vector<int> res;
+        if(!root)
+        return res;
+        if(!isLeaf(root))
         res.push_back(root->data);
-        return;
+        addLeftBoundary(root, res);
+        addLeaves(root, res);
+        addRightBoundary(root, res);
+        return res;
     }
-    if(root->left) addLeaves(root->left, res);
-    if(root->right) addLeaves(root->right, res);
-}
+};
