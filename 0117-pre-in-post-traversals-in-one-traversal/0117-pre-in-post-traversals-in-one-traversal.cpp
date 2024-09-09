e160fb20c834f652df27f8a0ea5ef0c00fa45950
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-09 18:33:13 +0530

    Time: 3 ms (95.37%), Space: 11.2 MB (70.12%) - LeetHub

--- 0117-pre-in-post-traversals-in-one-traversal/0117-pre-in-post-traversals-in-one-traversal.cpp
+++ 0117-pre-in-post-traversals-in-one-traversal/0117-pre-in-post-traversals-in-one-traversal.cpp
@@ -1,4 +1,31 @@
 vector<vector<int>> getTreeTraversal(TreeNode *root){
-    // Write your code here.
-    
+    vector<int>pre,in,post;
+    if(root==NULL)
+    return{};
+    stack<pair<TreeNode *,int>> st;
+    st.push({root,1});
+    while(!st.empty()){
+        auto it=st.top();
+        st.pop();
+        if(it.second==1){
+            pre.push_back(it.first->data);
+            it.second=2;
+            st.push(it);
+            if(it.first->left!=NULL) st.push({it.first->left,1});
+        }
+        else if(it.second==2){
+            in.push_back(it.first->data);
+            it.second=3;
+            st.push(it);
+            if(it.first->right!=NULL) st.push({it.first->right,1});
+        }
+        else{
+            post.push_back(it.first->data);
+        }
+    }
+    vector<vector<int>> ans;
+    ans.push_back(in);
+    ans.push_back(pre);
+    ans.push_back(post);
+    return ans;
 }
