407bcf22c46258424cd97325d8fb5af34253d587
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-11 21:44:28 +0530

    Time: 3 ms (95.37%), Space: 11.2 MB (70.12%) - LeetHub

--- 0211-children-sum-property/0211-children-sum-property.cpp
+++ 0211-children-sum-property/0211-children-sum-property.cpp
@@ -1,8 +1,25 @@
 void changeTree(BinaryTreeNode < int > * root) {
-    // Write your code here.
     if(root==NULL)
     return;
     int child=0;
     if(root->left)
         child+=root->left->data;
-    }  
+    if(root->right)
+        child+=root->right->data;
+    if(child>=root->data){
+        root->data=child;
+    }
+    else{
+        if(root->left)
+            root->left->data=root->data;
+        if(root->right)
+            root->right->data=root->data;
+    }
+    changeTree(root->left);
+    changeTree(root->right);
+    int total=0;
+    if(root->left) total+=root->left->data;
+    if(root->right) total+=root->right->data;
+    if(root->left || root->right)
+        root->data=total;
+}  
