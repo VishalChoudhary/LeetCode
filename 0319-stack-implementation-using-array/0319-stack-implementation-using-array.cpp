d334c91912ca99695307aaea3aa8834bdef4914f
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-13 21:22:56 +0530

    Time: 3 ms (95.37%), Space: 11.2 MB (70.12%) - LeetHub

--- 0319-stack-implementation-using-array/0319-stack-implementation-using-array.cpp
+++ 0319-stack-implementation-using-array/0319-stack-implementation-using-array.cpp
@@ -2,6 +2,52 @@ class Stack {
     
 public:
     
-   
+    int t=-1;
+    int n=0;
+    int *arr;
+
+    Stack(int capacity) {
+        // Write your code here.
+        n=capacity;
+        arr=new int[capacity];
+    }
+
+    void push(int num) {
+        // Write your code here.
+        if(t==n-1)
+        return;
+        t++;
+        arr[t]=num;
+    }
+
+    int pop() {
+        // Write your code here.
+        if(t==-1)
+        return -1;
+        int temp=arr[t];
+        t--;
+        return temp;
+    }
+    
+    int top() {
+        // Write your code here.
+        if(t==-1)
+        return -1;
+        return arr[t];
+    }
+    
+    int isEmpty() {
+        // Write your code here.
+        if(t==-1)
+        return 1;
+        return 0;
+    }
+    
+    int isFull() {
+        // Write your code here.
+        if(t==n-1)
+        return 1;
+        return 0;
+    }
     
 };
