02da8e25a408658bbeb72590a1b5829a5df47069
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-14 17:30:53 +0530

    Time: 3 ms (95.37%), Space: 11.2 MB (70.12%) - LeetHub

--- 0414-queue-implementation-using-array/0414-queue-implementation-using-array.cpp
+++ 0414-queue-implementation-using-array/0414-queue-implementation-using-array.cpp
@@ -11,4 +11,41 @@ public:
 		arr.resize(100001);
 	}
 
-	};
+	// Enqueue (add) element 'e' at the end of the queue.
+	void enqueue(int e)
+	{
+		// Write your code here.
+		if(rear==arr.size()-1)
+		return;
+		if(front==-1 && rear==-1){
+			front=0;
+			rear=0;
+			arr[rear]=e;
+		}
+		else{
+			rear++;
+			arr[rear]=e;
+		}
+	}
+
+	// Dequeue (retrieve) the element from the front of the queue.
+	int dequeue()
+	{
+		// Write your code here.
+		if(front==-1 && rear==-1)
+		return -1;
+		int temp=arr[front];
+		if(front==rear){
+			front=-1;
+			rear=-1;
+		}
+		else{
+			front++;
+		}
+		return temp;
+	}
+
+	int top(){
+		return arr[front];
+	}
+};
