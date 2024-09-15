6f41d51f97cf8644572e3292428bc4330787d7c8
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-15 22:57:23 +0530

    Time: 3 ms (95.37%), Space: 11.2 MB (70.12%) - LeetHub

--- 0291-sort-a-stack/0291-sort-a-stack.cpp
+++ 0291-sort-a-stack/0291-sort-a-stack.cpp
@@ -1,1 +1,14 @@
-1231-circular-queue-implementation-using-array
+#include <bits/stdc++.h> 
+void sortStack(stack<int> &stack)
+{
+	// Write your code here
+	vector<int> ds;
+	while(!stack.empty()){
+		ds.push_back(stack.top());
+		stack.pop();
+	}
+	sort(ds.begin(),ds.end());
+	for (int i : ds) {
+        stack.push(i);
+    }
+}
