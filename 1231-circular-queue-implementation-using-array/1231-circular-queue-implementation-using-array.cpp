fbe97f1d7cb4eb0b013ad02bf025e035a7f651d2
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-15 23:00:12 +0530

    Time: 0 ms (100%), Space: 8.4 MB (9.08%) - LeetHub

--- 1231-circular-queue-implementation-using-array/1231-circular-queue-implementation-using-array.cpp
+++ 1231-circular-queue-implementation-using-array/1231-circular-queue-implementation-using-array.cpp
@@ -1,3 +1,45 @@
 #include <bits/stdc++.h> 
 class CircularQueue{
-    };
+    public:
+    // Initialize your data structure.
+    int front;
+    int rear;
+    int *arr;
+    int size;
+    CircularQueue(int n){
+        // Write your code here.
+        size=n;
+        arr=new int[size];
+        front=rear=-1;
+    }
+
+    // Enqueues 'X' into the queue. Returns true if it gets pushed into the stack, and false otherwise.
+    bool enqueue(int value){
+        // Write your code here.
+        if((rear+1)%size==front)
+        return false;
+        else if(front==-1 && rear==-1){
+        front=rear=0;
+        arr[rear]=value;
+        }
+        else{
+            rear=(rear+1)%size;
+            arr[rear]=value;
+        }
+        return true;
+    }
+
+    // Dequeues top element from queue. Returns -1 if the stack is empty, otherwise returns the popped element.
+    int dequeue(){
+        // Write your code here.
+        if(front==-1 && rear==-1)
+        return -1;
+        int temp=arr[front];
+        if(rear==front)
+        front=rear=-1;
+        else{
+            front=(front+1)%size;
+        }
+        return temp;
+    }
+};
