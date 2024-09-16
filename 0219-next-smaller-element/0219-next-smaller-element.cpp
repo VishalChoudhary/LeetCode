39490f3bac12b452ab262240dd7ee54d5d2e5428
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-16 16:25:01 +0530

    Time: 3 ms (69.52%), Space: 11.4 MB (29.47%) - LeetHub

--- 0219-next-smaller-element/0219-next-smaller-element.cpp
+++ 0219-next-smaller-element/0219-next-smaller-element.cpp
@@ -1,4 +1,17 @@
 #include <stack>
 vector<int> nextSmallerElement(vector<int> &arr, int n)
 {
+    // Write your code here.
+    vector<int> ans(n,-1);
+    stack<int> st;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && st.top()>=arr[i]){
+            st.pop();
+        }
+        if(!st.empty()){
+            ans[i]=st.top();
+        }
+        st.push(arr[i]);
     }
+    return ans;
+}
