b398087bdb0495f0e00769ccbbb68c03ef6eddc2
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-20 00:08:20 +0530

    Time: 3 ms (90.37%), Space: 21.2 MB (70.12%) - LeetHub

--- 0333-ceil-the-floor/0333-ceil-the-floor.cpp
+++ 0333-ceil-the-floor/0333-ceil-the-floor.cpp
@@ -1,5 +1,36 @@
 #include<bits/stdc++.h>
 
+int ceil(vector<int> &arr,int n,int x){
+	int ans=-1;
+	int low=0,high=n-1;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]>=x){
+			ans=arr[mid];
+			high=mid-1;
+		}
+		else{
+			low=mid+1;
+		}
+	}
+	return ans;
+}
+
+int floor(vector<int> &arr,int n,int x){
+	int ans=-1;
+	int low=0,high=n-1;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]<=x){
+			ans=arr[mid];
+			low=mid+1;
+		}
+		else{
+			high=mid-1;
+		}
+	}
+	return ans;
+}	
 
 pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x) {
 	// Write your code here.
