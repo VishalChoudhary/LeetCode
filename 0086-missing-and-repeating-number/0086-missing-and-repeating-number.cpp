2365ef9de3257e52a64384e94af4bfcb59a9a899
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-21 21:35:38 +0530

    Time: 11 ms (84.95%), Space: 8.3 MB (94.58%) - LeetHub

--- 0086-missing-and-repeating-number/0086-missing-and-repeating-number.cpp
+++ 0086-missing-and-repeating-number/0086-missing-and-repeating-number.cpp
@@ -1,3 +1,22 @@
 vector<int> findMissingRepeatingNumbers(vector < int > a) {
-    
+    long long n = a.size();
+    long long SN = (n * (n + 1)) / 2;
+    long long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+    long long S = 0, S2 = 0;
+    for (int i = 0; i < n; i++) {
+        S += a[i];
+        S2 += (long long)a[i] * (long long)a[i];
+    }
+
+    long long val1 = S - SN;
+
+    long long val2 = S2 - S2N;
+
+    val2 = val2 / val1;
+
+    long long x = (val1 + val2) / 2;
+    long long y = x - val1;
+
+    return {(int)x, (int)y};
 }
