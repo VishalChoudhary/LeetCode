d501c15d508d9785e793e068aab0b1264af97c6b
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-22 20:41:52 +0530

    Time: 3 ms (95.37%), Space: 11.2 MB (70.12%) - LeetHub

--- 0377-aggresive-cows/0377-aggresive-cows.cpp
+++ 0377-aggresive-cows/0377-aggresive-cows.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 bool canWePlace(vector<int> &stalls,int dist,int cows){
-}    
+    int cntCows=1,last=stalls[0];
+    for(int i=1;i<stalls.size();i++){
+        if(stalls[i]-last>=dist){
+            cntCows++;
+            last=stalls[i];
+        }
+        if(cntCows>=cows)
+            return true;
+    }
+    return false;
+}
+
 int aggressiveCows(vector<int> &stalls, int k)
 {   
     sort(stalls.begin(),stalls.end());
