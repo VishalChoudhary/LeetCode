99a1884e2ef540b54eb8e1c572b5eccf770e04b9
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-23 00:23:06 +0530

    Time: 19 ms (84.8%), Space: 94.6 MB (87.7%) - LeetHub

--- 0216-min-heap/0216-min-heap.cpp
+++ 0216-min-heap/0216-min-heap.cpp
@@ -1,3 +1,17 @@
 #include <bits/stdc++.h> 
 vector<int> minHeap(int n, vector<vector<int>>& q) {
+    priority_queue<int,vector<int>,greater<int>> pq;
+    vector<int> ans;
+    for(int i=0;i<n;i++){
+        if(q[i][0]==0){
+            pq.push(q[i][1]);
+        }
+        else if(q[i][0]==1){
+            if(!q.empty()){
+                ans.push_back(pq.top());
+                pq.pop();
+            }
+        }
     }
+    return ans;
+}
