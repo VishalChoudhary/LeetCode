bbd9f593e98fca4b88eeff94f747f5235ba33159
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-25 08:32:09 +0530

    Time: 8 ms (42.65%), Space: 16.6 MB (100%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                ans.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2!=0){
+                ans.push_back(nums[i]);
+            }
+        }
+        // reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
