84ae8206cc8390a20d79365ff463acf3a5ab8ac3
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-09-29 22:36:09 +0530

    Time: 3 ms (90.37%), Space: 21.2 MB (70.12%) - LeetHub

--- 0471-build-max-heap/0471-build-max-heap.cpp
+++ 0471-build-max-heap/0471-build-max-heap.cpp
@@ -1,4 +1,19 @@
-
+#include <bits/stdc++.h> 
+void heapify(vector<int> &arr,int n,int i){
+    int left=2*i+1;
+    int right=2*i+2;
+    int largest=i;
+    if(left<n && arr[largest]<arr[left]){
+        largest=left;
+    }
+    if(right<n && arr[largest]<arr[right]){
+        largest=right;
+    }
+    if(largest!=i){
+        swap(arr[largest],arr[i]);
+        heapify(arr, n, largest);
+    }
+}
 vector<int> buildHeap(vector<int> arr, int n)
 {	
 	// Write your code here 
