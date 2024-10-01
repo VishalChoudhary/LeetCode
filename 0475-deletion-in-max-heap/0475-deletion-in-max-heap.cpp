04295612e0bb2f5b2849f5279e714447647e62d4
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-01 22:35:35 +0530

    Time: 3 ms (95.37%), Space: 11.2 MB (70.12%) - LeetHub

--- 0475-deletion-in-max-heap/0475-deletion-in-max-heap.cpp
+++ 0475-deletion-in-max-heap/0475-deletion-in-max-heap.cpp
@@ -1,12 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void heapify(vector<int> &heap, int n,int index){
+    int left=2*index+1;
+    int right=2*index+2;
+    int largest=index;
+    if(left<n && heap[left]>heap[largest]){
+        largest=left;
+    }
+    if(right<n && heap[right]>heap[largest])
+    largest=right;
+    if(largest!=index){
+        swap(heap[largest],heap[index]);
+        heapify(heap,n,largest);
+    }
+}
 
 int removeMax(vector<int> &heap,int n){
     if(heap.size()==0)
     return -1;
-    int root=heap[0];
-    heap[0]=heap.back();
-    heap.pop_back();
-    heapify(heap,n,0);
+    int root=heap[0];    // The root is the largest element
+    heap[0]=heap.back();   // Move the last element to the root
+    heap.pop_back();    // Remove the last element
+    heapify(heap,n,0);  // Heapify down from the root to restore the heap property
     return root;
 }
 void printHeap(const vector<int>& heap) {
@@ -16,18 +32,12 @@ void printHeap(const vector<int>& heap) {
 }
 
 int main() {
-    // A max-heap represented as a vector
     vector<int> heap = {50, 30, 40, 20, 10, 15, 45};
-
     cout << "Max-Heap before removal: ";
     printHeap(heap);
-
-    // Remove the root element (50) from the heap
     int removed = removeMax(heap,heap.size());
     cout << "Removed element: " << removed << endl;
-
     cout << "Max-Heap after removal: ";
     printHeap(heap);
-
     return 0;
 }
