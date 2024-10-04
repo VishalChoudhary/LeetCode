cd991dfa4d8b7ad5dafdff89cd2ee7708eba865a
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-04 23:16:06 +0530

    Time: 8 ms (32.34%), Space: 12.4 MB (24.39%) - LeetHub

--- 0519-pairs-with-positive-negative/0519-pairs-with-positive-negative.cpp
+++ 0519-pairs-with-positive-negative/0519-pairs-with-positive-negative.cpp
@@ -1,5 +1,25 @@
 class Solution {
   public:
     vector<int> posNegPair(vector<int> &arr) {
-           }
+        // Code here
+        vector<int> ans,v;
+        map<int,int> mpp;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]<0)
+                v.push_back(arr[i]);
+            else
+                mpp[arr[i]]++;
+        }
+        sort(v.begin(),v.end());
+        for(int i=(int)v.size()-1;i>=0;i--){
+            int data=abs(v[i]);
+            if(mpp[data]>0){
+                ans.push_back(v[i]);
+                ans.push_back(data);
+                mpp[data]--;
+            }
+        }
+        return ans;
+    }
 };
