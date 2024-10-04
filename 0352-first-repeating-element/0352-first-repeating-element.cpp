0d6b5a61e9946743429419621b5fef96a99c1ee2
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-04 17:49:34 +0530

    Time: 3 ms (69.52%), Space: 11.4 MB (29.47%) - LeetHub

--- 0352-first-repeating-element/0352-first-repeating-element.cpp
+++ 0352-first-repeating-element/0352-first-repeating-element.cpp
@@ -1,6 +1,19 @@
 class Solution {
   public:
     int firstRepeated(vector<int> &nums) {
-        
+        // code here
+        int n=nums.size();
+        unordered_map<int,int> umap;
+        for(int i=0;i<n;i++){
+            umap[nums[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            auto it=umap.find(nums[i]);
+            if(it->second>1){
+                return i+1;
+                break;
+            }
+        }
+        return -1;
     }
 };
