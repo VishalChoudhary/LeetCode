a203059190704aaf2ebad798aebd7aba7927098b
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-08 22:24:47 +0530

    Add files via upload

---
+++ 0239-predecessor-and-successor-of-binary-search-tree/0239-predecessor-and-successor-of-binary-search-tree.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+    public:
+    void findPreSuc(Node* root, Node*& pre, Node*& suc, int p)
+    {
+        // Your code goes here
+        Node *curr=root;
+        while(curr!=NULL){
+            if(p>=curr->val)
+        
+};
