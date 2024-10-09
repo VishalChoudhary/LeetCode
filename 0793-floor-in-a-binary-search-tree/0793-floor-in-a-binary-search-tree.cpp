88b0911c08d960010ca297ea236469294cee0ac3
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-09 15:17:11 +0530

    Time: 0 ms (100%), Space: 9.1 MB (60.85%) - LeetHub

--- 0793-floor-in-a-binary-search-tree/0793-floor-in-a-binary-search-tree.cpp
+++ 0793-floor-in-a-binary-search-tree/0793-floor-in-a-binary-search-tree.cpp
@@ -2,5 +2,18 @@ int floorInBST(TreeNode<int> * root, int key)
 {
     // Write your code here.
     int floor=-1;
-    
+    while(root){
+        if(root->val==key){
+            floor=root->val;
+            return floor;
+        }
+        if(root->val>key){
+            root=root->left;
+        }
+        else{
+            floor=root->val;
+            root=root->right;
+        }
+    }
+    return floor;
 }
