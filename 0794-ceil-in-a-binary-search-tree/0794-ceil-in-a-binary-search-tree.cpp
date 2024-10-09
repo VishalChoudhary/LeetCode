4433d37e605bd1cdaeee5a904716c85ae76f7013
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-09 15:19:37 +0530

    Time: 0 ms (100%), Space: 9.1 MB (60.85%) - LeetHub

--- 0794-ceil-in-a-binary-search-tree/0794-ceil-in-a-binary-search-tree.cpp
+++ 0794-ceil-in-a-binary-search-tree/0794-ceil-in-a-binary-search-tree.cpp
@@ -1,4 +1,18 @@
 int findCeil(BinaryTreeNode<int> *root, int key){
     // Write your code here.
     int ceil=-1;
-   }
+    while(root){
+        if(root->data==key){
+            ceil=root->data;
+            return ceil;
+        }
+        if(root->data<key){
+            root=root->right;
+        }
+        else{
+            ceil=root->data;
+            root=root->left;
+        }
+    }
+    return ceil;
+}
