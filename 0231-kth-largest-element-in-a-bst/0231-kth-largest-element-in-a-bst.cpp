334cb140bba9f3b4f4c132a1b0ec91c907b625ed
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-09 20:07:40 +0530

    Time: 11 ms (84.95%), Space: 8.3 MB (94.58%) - LeetHub

--- 0231-kth-largest-element-in-a-bst/0231-kth-largest-element-in-a-bst.cpp
+++ 0231-kth-largest-element-in-a-bst/0231-kth-largest-element-in-a-bst.cpp
@@ -1,6 +1,17 @@
 class Solution
 {
-
+    public:
+    void reverseInOrder(Node *root,int &counter,int k,int &kLargest){
+        if(root==NULL || counter>=k)
+        return;
+        reverseInOrder(root->right,counter,k,kLargest);
+        counter++;
+        if(counter==k){
+            kLargest=root->data;
+            return;
+        }
+        reverseInOrder(root->left,counter,k,kLargest);
+    }
     int kthLargest(Node *root, int K)
     {
         //Your code here
