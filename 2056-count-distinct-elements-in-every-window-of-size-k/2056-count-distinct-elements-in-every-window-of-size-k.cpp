f5072ca20270194516942d389b65c0818a102d02
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-11 18:48:29 +0530

    Time: 7 ms (16.86%), Space: 9.1 MB (24.35%) - LeetHub

--- 2056-count-distinct-elements-in-every-window-of-size-k/2056-count-distinct-elements-in-every-window-of-size-k.cpp
+++ 2056-count-distinct-elements-in-every-window-of-size-k/2056-count-distinct-elements-in-every-window-of-size-k.cpp
@@ -2,7 +2,17 @@ class Solution{
   public:
     vector <int> countDistinct (int A[], int n, int k)
     {
-                        umap[A[i-k]]--;
+        unordered_map<int,int> umap;
+        for(int i=0;i<k;i++){
+            umap[A[i]]++;
+        }
+        vector<int> ans;
+        ans.push_back(umap.size());
+        for(int i=k;i<n;i++){
+            if(umap[A[i-k]]==1)
+                umap.erase(A[i-k]);
+            else
+                umap[A[i-k]]--;
             umap[A[i]]++;
             ans.push_back(umap.size());
         }
