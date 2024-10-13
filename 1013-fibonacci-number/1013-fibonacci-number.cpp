dcf6433ac0c511a94ed09604e62bf0d899795e47
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-13 17:03:12 +0530

    Time: 2 ms (80.2%), Space: 7.5 MB (33.61%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,15 +1,16 @@
-//using tabulation
+//using tabulation with space optimization
 class Solution {
 public:
     int fib(int n) {
         if(n<=1)
         return n;
-        vector<int> dp(n+1,0);
-        dp[0]=0;
-        dp[1]=1;
+        int prev2=0;
+        int prev=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n];
+        return prev; // it will run for n+1 times so the res will be stored in prev
     }
 };
