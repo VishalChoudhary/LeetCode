61bc47cfb156e0527f6c3f8d5e43505a1c09c169
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-14 19:05:15 +0530

    Time: 3 ms (39.34%), Space: 8.3 MB (38.03%) - LeetHub

--- 0672-frog-jump-k-distance-or-minimal-cost/0672-frog-jump-k-distance-or-minimal-cost-memoization.cpp
+++ 0672-frog-jump-k-distance-or-minimal-cost/0672-frog-jump-k-distance-or-minimal-cost-memoization.cpp
@@ -6,5 +6,16 @@ class Solution {
         if(dp[n]!=-1) return dp[n];
         for(int j=1; j<=k; j++){
             if(n-j>=0){
-
+                int jump=f(n-j,dp,k,arr)+abs(arr[n]-arr[n-j]);
+                minSteps=min(jump,minSteps);
+            }
+        }
+        return dp[n]=minSteps;
+    }
+    int minimizeCost(int k, vector<int>& arr) {
+        // Code here
+        int n=arr.size();
+        vector<int> dp(n+1,-1);
+        return f(n-1,dp,k,arr);
+    }
 };
