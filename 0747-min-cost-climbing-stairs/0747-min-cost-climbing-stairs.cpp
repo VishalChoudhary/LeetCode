20062cc0838a1a6fea9f126c55906647a1acb1b6
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-14 16:24:17 +0530

    Time: 3 ms (80.58%), Space: 16.8 MB (53.37%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,18 +1,18 @@
-//memoization
+// memoization
 class Solution {
 public:
-    int f(int n, vector<int>& cost, vector<int>& dp) {
-        if (n == 0 || n==1)
-            return 0;
-        if (dp[n] != -1)
-            return dp[n];
-        int left = f(n - 1, cost, dp) + cost[n - 1];
-        int right = f(n - 2, cost, dp) + cost[n - 2];
-        return dp[n] = min(left, right);
-    }
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n + 1, -1);
-        return f(n, cost, dp);
+        int n=cost.size();
+        vector<int> dp(n+1, 0);
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i <=n; i++) {
+            int left = dp[i - 1] + cost[i - 1];
+            int right = INT_MAX;
+            if (i > 1)
+                right = dp[i - 2] +  cost[i - 2];
+            dp[i] = min(left, right);
+        }
+        return dp[n];
     }
 };
