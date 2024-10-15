0ee45462ec6e1dbb7e424fbaca76b8552e88ea16
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-15 19:45:06 +0530

    Time: 4 ms (32.22%), Space: 9.6 MB (11.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,7 +4,6 @@ public:
         int n=nums.size();
         vector<int> dp(n,0);
         dp[0]=nums[0];
-        int neg=0;
         for(int i=1;i<n;i++){
            int pick = nums[i];
            if(i>1) pick+=dp[i-2];
