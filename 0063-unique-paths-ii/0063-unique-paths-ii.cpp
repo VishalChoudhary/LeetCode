1ab55aa9b63714da7b58677e7d8b1821366821b8
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-18 23:46:51 +0530

    Time: 0 ms (100%), Space: 10.7 MB (6.64%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -7,18 +7,18 @@ public:
         //if we face any obstacle at start no path possible
         if(obstacleGrid[0][0]==1)
         return 0;
-         dp[0][0] = 1;
-
-        // Fill the dp table
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0; // Obstacle cell
-                } else {
-                    if (i > 0) 
-                        dp[i][j] += dp[i-1][j]; // From the top
-                    if (j > 0) 
-                        dp[i][j] += dp[i][j-1]; // From the left
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                //if we face any obstacle
+                else if(obstacleGrid[i][j]==1)
+                    dp[i][j]=0;
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
                 }
             }
         }
