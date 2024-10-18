63feb4eaae5d73c9d1cadf1112bdadfda70324e1
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-18 23:43:48 +0530

    Time: 0 ms (100%), Space: 10.4 MB (66.47%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    //memoization
-    int f(int i,int j,vector<vector<int>> &dp,vector<vector<int>>& obstacleGrid){
-        // If the starting point has an obstacle, no paths are possible
-        if (obstacleGrid[0][0] == 1) 
-            return 0;
-        // Base case: If we reach the top-left corner (0, 0), there is one way.
-        if(i==0 && j==0)
-            return 1;
-        // If we go out of bounds
-        if(i<0 || j<0) 
-            return 0;
-        //if we face any obstacle
-        if(i>=0 && j>=00 && obstacleGrid[i][j]==1)
-            return 0;
-        if (dp[i][j]!=-1)
-            return dp[i][j];
-        int up=f(i-1,j,dp,obstacleGrid);
-        int left=f(i,j-1,dp,obstacleGrid);
-        return dp[i][j]=up+left;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,dp,obstacleGrid);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        //if we face any obstacle at start no path possible
+        if(obstacleGrid[0][0]==1)
+        return 0;
+         dp[0][0] = 1;
+
+        // Fill the dp table
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0; // Obstacle cell
+                } else {
+                    if (i > 0) 
+                        dp[i][j] += dp[i-1][j]; // From the top
+                    if (j > 0) 
+                        dp[i][j] += dp[i][j-1]; // From the left
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
