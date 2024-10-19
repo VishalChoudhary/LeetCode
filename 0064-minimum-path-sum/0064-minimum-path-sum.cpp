010d2ba1b611c313f0e9f0a1376f91d69c163964
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-19 16:53:10 +0530

    Time: 6 ms (93.76%), Space: 14.8 MB (62.59%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    //tabulation
+    //space optimization
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        vector<int> prev(n,0);
         for(int i=0;i<m;i++){
+            vector<int> curr(n,0);
             for(int j=0;j<n;j++){
                 if(i==0 && j==0)
-                    dp[i][j]=grid[i][j];
+                    curr[j]=grid[i][j];
                 else{
                     int up=INT_MAX;
                     int left=INT_MAX;
-                    if(i>0) up=grid[i][j]+dp[i-1][j];
-                    if(j>0) left=grid[i][j]+dp[i][j-1];                
-                    dp[i][j]=min(left,up);
+                    if(i>0) up=grid[i][j]+prev[j];
+                    if(j>0) left=grid[i][j]+curr[j-1];                
+                    curr[j]=min(left,up);
                 }
             }
+            prev=curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
