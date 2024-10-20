c339c97b12e0c5f00f135e59497af706f8fd887f
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-20 16:53:34 +0530

    Time: 0 ms (100%), Space: 5.9 MB (100%) - LeetHub

--- 0968-maximum-falling-path/0968-maximum-falling-path-space-optimization.cpp
+++ 0968-maximum-falling-path/0968-maximum-falling-path-space-optimization.cpp
@@ -8,4 +8,20 @@ int getMaxPathSum(vector<vector<int>> &matrix)
     for(int j=0;j<m;j++){
         prev[j]=matrix[0][j];
     }
+    for(int i=1;i<n;i++){
+        for(int j=0;j<m;j++){
+            int up=matrix[i][j]+prev[j];
+            int ld=INT_MIN;
+            if(j-1>=0) ld=matrix[i][j]+prev[j-1];
+            int rd=INT_MIN;
+            if(j+1<m) rd=matrix[i][j]+prev[j+1];
+            curr[j]=max(up,max(ld,rd));
+        }
+        prev=curr;
     }
+    int maxi=INT_MIN;
+    for(int j=0;j<m;j++){
+        maxi=max(maxi,prev[j]);
+    }
+    return maxi;
+}
