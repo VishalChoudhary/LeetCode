0a236e67b5f198997fa6ebe7f72fce8baa192ebc
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-21 19:46:11 +0530

    Time: 2 ms (97%), Space: 8.7 MB (76.37%) - LeetHub

--- 0970-subset-sum-equal-to-k/0970-subset-sum-equal-to-k-tabulation.cpp
+++ 0970-subset-sum-equal-to-k/0970-subset-sum-equal-to-k-tabulation.cpp
@@ -3,7 +3,7 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
     vector<vector<bool>> dp(n, vector<bool>(k+1,0));
     //base case
     for(int i=0;i<n;i++) dp[i][0]=true;
-    dp[0][arr[0]]=true;
+    if(arr[0]<=k) dp[0][arr[0]]=true;
     for(int ind=1;ind<n;ind++){
         for(int target=1;target<=k;target++){
             bool notTake=dp[ind-1][target];
