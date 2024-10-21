2acafda00e703a13a360e76025beb7b0a1d36d4c
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-21 19:33:31 +0530

    Time: 331 ms (18.87%), Space: 16.3 MB (76.31%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
-    //memoization
-    int f(int ind, int target,vector<int> &arr,vector<vector<int>> &dp){
-        //base case
-        if(target==0) return true;
-        if(ind==0) return (arr[0]==target);
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        bool notTake=f(ind-1,target,arr,dp);
-        bool take=false;
-        if(arr[ind]<=target) take=f(ind-1,target-arr[ind],arr,dp);
-        return dp[ind][target]=take | notTake;
-    }
+    //tabulation
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int totalSum=0;
@@ -20,7 +10,18 @@ public:
         //odd no case
         if(totalSum%2!=0) return false;
         int k=totalSum/2;
-        vector<vector<int>> dp(n, vector<int>(k+1,-1));
-        return f(n-1,k,nums,dp);
+        vector<vector<bool>> dp(n, vector<bool>(k+1,0));
+        //base case
+        for(int i=0;i<n;i++) dp[i][0]=true;
+        if(nums[0]<=k) dp[0][nums[0]]=true;
+        for(int ind=1;ind<n;ind++){
+            for(int target=1;target<=k;target++){
+                bool notTake=dp[ind-1][target];
+                bool take=false;
+                if(nums[ind]<=target) take=dp[ind-1][target-nums[ind]];
+                dp[ind][target]=take | notTake;
+            }
+        }
+        return dp[n-1][k];
     }
 };
