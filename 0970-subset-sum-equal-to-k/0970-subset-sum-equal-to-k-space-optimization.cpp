f2dd8edf8116990052edc8d21968a9f01ce20073
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-21 19:46:46 +0530

    Time: 0 ms (100%), Space: 8.7 MB (76.37%) - LeetHub

--- 0970-subset-sum-equal-to-k/0970-subset-sum-equal-to-k-space-optimization.cpp
+++ 0970-subset-sum-equal-to-k/0970-subset-sum-equal-to-k-space-optimization.cpp
@@ -3,7 +3,7 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
     vector<bool> prev(k+1,0), curr(k+1,0);
     //base case
     prev[0]=curr[0]=true;
-    prev[arr[0]]=true;
+    if(arr[0]<=k) prev[arr[0]]=true;
     for(int ind=1;ind<n;ind++){
         for(int target=1;target<=k;target++){
             bool notTake=prev[target];
