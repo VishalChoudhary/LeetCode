2fe012c262a6b048cadc2d4aa4dc2c2505bd7b4f
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-22 23:46:15 +0530

    Time: 74 ms (14.32%), Space: 20.2 MB (12.40%) - LeetHub

--- 0417-count-subsets-with-sum-k/0417-count-subsets-with-sum-k-space-optimization.cpp
+++ 0417-count-subsets-with-sum-k/0417-count-subsets-with-sum-k-space-optimization.cpp
@@ -4,4 +4,20 @@ int findWays(vector<int>& arr, int k)
 	int n=arr.size();
 	vector<int> prev(k+1,0), curr(k+1,0);
 	prev[0]=1;
-	
+	for(int sum=0;sum<=k;sum++){
+		if(arr[0]==0 && sum==0) 
+			prev[sum]=2;
+		else if(arr[0]==sum)
+			prev[sum]=1;
+	}
+	for(int ind=1;ind<n;ind++){
+		for(int sum=0;sum<=k;sum++){
+			int notPick=prev[sum];
+			int pick=0;
+			if(arr[ind]<=sum) pick=prev[sum-arr[ind]];
+			curr[sum]=(pick+notPick)%MOD;
+		}
+		prev=curr;
+	}
+	return prev[k];
+}
