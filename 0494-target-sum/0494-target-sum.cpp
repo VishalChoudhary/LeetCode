5dc71768df7877fe36007f1be0d242a704a69670
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-23 00:20:37 +0530

    Time: 6 ms (77.39%), Space: 13.9 MB (58.95%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    int f(int ind,int sum,vector<int>& arr,vector<vector<int>> &dp){
-        if(ind==0) {
-        if(sum==0) 
-            return(arr[ind]==0)?2:1;
-        else
-            return (arr[ind]==sum)?1:0;
-        }
-        if(dp[ind][sum]!=-1) return dp[ind][sum];
-        int notPick=f(ind-1,sum,arr,dp);
-        int pick=0;
-        if(arr[ind]<=sum) pick=f(ind-1,sum-arr[ind],arr,dp);
-        return dp[ind][sum]=pick+notPick;
-    }
+    //  tabulation
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         int totalSum=0;
         for(int i=0;i<n;i++){
             totalSum+=nums[i];
         }
-        if(totalSum-target <0 || (totalSum-target)%2!=0) return 0;
-        vector<vector<int>> dp(n, vector<int>((totalSum-target)/2+1,-1));
-        return f(n-1,(totalSum - target)/2,nums,dp);
-
+        if(totalSum < target || (totalSum-target)%2!=0) return 0;
+         int subsetSum = (totalSum-target)/2;
+        vector<vector<int>> dp(n, vector<int>(subsetSum+1,0));
+        dp[0][0]=1;
+        for(int sum=0;sum<=subsetSum;sum++){
+            if(nums[0]==0 && sum==0) dp[0][sum]=2;
+            else if(nums[0]==sum) dp[0][sum]=1;
+        }
+        for(int ind=1;ind<n;ind++){
+            for(int sum=0;sum<=subsetSum;sum++){
+                int notPick=dp[ind-1][sum];
+                int pick=0;
+                if(nums[ind]<=sum) pick=dp[ind-1][sum-nums[ind]];
+                dp[ind][sum]=pick+notPick;
+            }
+        }
+        return dp[n-1][(totalSum - target)/2];
     }
 };
