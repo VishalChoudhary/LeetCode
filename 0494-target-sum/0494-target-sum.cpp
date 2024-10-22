9ac61f9c5b3a54a0173bbab55cbb82933a74ca9c
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-23 01:49:58 +0530

    Time: 6 ms (77.26%), Space: 14 MB (55.92%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    //  space optimization
+    //  tabulation
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         int totalSum=0;
         for(int i=0;i<n;i++){
             totalSum+=nums[i];
         }
-        if(totalSum < target || (totalSum-target)%2!=0) return 0;
-        int subsetSum = (totalSum-target)/2;
-        vector<int> prev(subsetSum+1,0), curr(subsetSum+1,0);
-        prev[0]=1;
+        if(totalSum-target < 0 || (totalSum-target)%2!=0) return 0;
+         int subsetSum = (totalSum-target)/2;
+        vector<vector<int>> dp(n, vector<int>(subsetSum+1,0));
+        dp[0][0]=1;
         for(int sum=0;sum<=subsetSum;sum++){
-            if(nums[0]==0 && sum==0) prev[sum]=2;
-            else if(nums[0]==sum) prev[sum]=1;
+            if(nums[0]==0 && sum==0) dp[0][sum]=2;
+            else if(nums[0]==sum) dp[0][sum]=1;
         }
         for(int ind=1;ind<n;ind++){
             for(int sum=0;sum<=subsetSum;sum++){
-                int notPick=prev[sum];
+                int notPick=dp[ind-1][sum];
                 int pick=0;
-                if(nums[ind]<=sum) pick=prev[sum-nums[ind]];
-                curr[sum]=pick+notPick;
+                if(nums[ind]<=sum) pick=dp[ind-1][sum-nums[ind]];
+                dp[ind][sum]=pick+notPick;
             }
-            prev=curr;
         }
-        return prev[(totalSum - target)/2];
+        return dp[n-1][(totalSum - target)/2];
     }
 };
