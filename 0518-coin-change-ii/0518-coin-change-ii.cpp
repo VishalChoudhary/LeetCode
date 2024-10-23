a4e93c7639888f9d11f2aa44435c84b6facea9e0
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-10-23 18:06:09 +0530

    Time: 27 ms (22.75%), Space: 26.9 MB (32.69%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int f(int ind,int target, vector<int>& coins,vector<vector<int>> &dp){
-        if(ind==0)  
-            return (target%coins[0]==0);
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        int notTake=f(ind-1,target,coins,dp);
-        int take=0;
-        if(coins[ind]<=target) take=f(ind,target-coins[ind],coins,dp);
-        return dp[ind][target]=take+notTake;
-    }
     int change(int amount, vector<int>& coins) {
-       int n=coins.size();
-       vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-       return f(n-1,amount,coins,dp); 
+        //Using unsigned instead of int prevents signed integer overflow 
+        vector<vector<unsigned>> g(coins.size() + 1, vector<unsigned> (amount+1));
+        for(int i = 0 ; i < coins.size()+1; i++){
+            g[i][0] = 1;
+        }
+        for(int i = 1; i <= coins.size(); i++){
+            for(int j = 1; j <=amount; j++){
+                if(j - coins[i-1] < 0){
+                    g[i][j] = g[i-1][j];
+                }else{
+                    g[i][j] = g[i-1][j]+ g[i][j-coins[i-1]];
+                }
+            }
+        }
+        return g[coins.size()][amount];
     }
 };
