3746822951d8bf409656c052977330a905763c23
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-11-02 13:14:31 +0530

    Time: 6 ms (88.64%), Space: 11.7 MB (85%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int f(int ind1,int ind2,string &s1,string &s2,vector<vector<int>> &dp){     
-        if(ind1<0 || ind2<0)
-            return 0;
-        if(dp[ind1][ind2]!=-1)
-            return dp[ind1][ind2];
-        if(s1[ind1]==s2[ind2])
-            return dp[ind1][ind2]=1+f(ind1-1,ind2-1,s1,s2,dp);
-        return dp[ind1][ind2]=0+max(f(ind1-1,ind2,s1,s2,dp),f(ind1,ind2-1,s1,s2,dp));
-    }
-    int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size();
-        int m=text2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,text1,text2,dp);
+    int longestCommonSubsequence(string s1, string s2) {
+        int n=s1.size();
+        int m=s2.size();
+        vector<int> prev(m+1,0), curr(m+1,0);
+        for(int i=0;i<=n;i++) prev[0]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1])
+                    curr[j]=1+prev[j-1];
+                else curr[j]=max(prev[j],curr[j-1]);
+            }
+            prev=curr;
+        }
+        return prev[m];
     }
     int minDistance(string word1, string word2) {
         return word1.size()+word2.size()-2*longestCommonSubsequence(word1,word2);
