e3308a0f2f61fe1a052e1ad5d6b8227f6f1c2f67
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-11-02 17:24:13 +0530

    Time: 15 ms (89.59%), Space: 8 MB (94.54%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
+    //space optimization
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
+        vector<double> prev(m+1,0), curr(m+1,0);
         if (n < m) return 0;
-        for(int i=0;i<=n;i++) dp[i][0]=1;
+        //bcz we are using only two rows curr and prev so assigning 1 to both rows no need to run loop
+        prev[0]=curr[0]=1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s[i-1]==t[j-1]){  //comparing is 0 based indexing as string start from 0 index
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                    curr[j]=prev[j-1]+prev[j];
                 }
                 else
-                    dp[i][j]=dp[i-1][j];
+                    curr[j]=prev[j];
             }
+            prev=curr;
         }
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
