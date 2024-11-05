fb8f31b0edf579541d3f01e074371d85c2a278d0
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-11-05 18:05:18 +0530

    Time: 382 ms (46.85%), Space: 209.8 MB (52.06%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -8,14 +8,12 @@ public:
             for(int buy=0; buy<=1; buy++){
                 //reason for cap from 1 bcz cap=0 is base case which is already covered
                 for(int cap=1;cap<=2;cap++){ 
-                    int profit=0;
                     if(buy){
-                        profit=max(-prices[ind]+dp[ind+1][0][cap],0+dp[ind+1][1][cap]);
+                        dp[ind][buy][cap]=max(-prices[ind]+dp[ind+1][0][cap],0+dp[ind+1][1][cap]);
                     }
                     else{
-                        profit=max(prices[ind]+dp[ind+1][1][cap-1],0+dp[ind+1][0][cap]);
+                        dp[ind][buy][cap]=max(prices[ind]+dp[ind+1][1][cap-1],0+dp[ind+1][0][cap]);
                     }   
-                    dp[ind][buy][cap]=profit;
                 }  
             }
         }
