a5f22935619ee83f9939d4bc0ad425a70d536327
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-11-08 21:03:05 +0530

    Time: 2 ms (16.64%), Space: 8 MB (20.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    //space optimisation
+    //memoization
+    int f(int i,int j,vector<vector<int>> &dp){
+        // Base case: If we reach the top-left corner (0, 0), there is one way.
+        if(i==0 && j==0)
+            return 1;
+        // If we go out of bounds
+        if(i<0 || j<0) 
+            return 0;
+        if (dp[i][j]!=-1)
+            return dp[i][j];
+        int up=f(i-1,j,dp);
+        int left=f(i,j-1,dp);
+        return dp[i][j]=up+left;
+    }
     int uniquePaths(int m, int n) {
-        vector<int> prev(n,0);
-        for(int i=0;i<m;i++){
-            vector<int> curr(n,0);
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0)
-                    curr[j]=1;
-                else{
-                    int up=0;
-                    int left=0;
-                    if(i>0) up=prev[j];
-                    if(j>0) left=curr[j-1];
-                    curr[j]=up+left;
-                }
-            }
-            prev=curr;
-        }
-        return prev[n-1];
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return f(m-1,n-1,dp);
     }
 };
