0655f366ece1151473206571a36215457069cb95
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-11-09 13:15:02 +0530

    Time: 103 ms (55.75%), Space: 74.5 MB (68.72%) - LeetHub

---
+++ 0301-max-sum-increasing-subsequence/0301-max-sum-increasing-subsequence-tabluation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+int maxIncreasingDumbbellsSum(vector<int> &nums, int n)
+{
+    vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
+    if(n==0) return 0;
+    for(int ind=n-1;ind>=0;ind--){
+        for(int prev=n-1;prev>=-1;prev--){
+            int notTake=0+dp[ind+1][prev+1];
+            int take=0;
+            if(prev==-1 || nums[ind]>nums[prev]){
+                take=nums[ind]+dp[ind+1][ind+1];
+            }
+            dp[ind][prev+1]=max(take,notTake);
+        }
+    }
+    return dp[0][0];
+}
