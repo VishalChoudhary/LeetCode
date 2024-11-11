eb7b87927f89249dbafc5937ad6bf2eb8b6f40be
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-11-11 16:05:39 +0530

    Time: 198 ms (31.57%), Space: 104 MB (32.73%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -11,18 +11,6 @@ public:
         }
         return j == s2.size();
     }
-    int f(int ind,int prev,int n,vector<string>& nums,vector<vector<int>> &dp){
-        //base case
-        if(ind==n) return 0;
-        if(dp[ind][prev+1]!=-1)
-            return dp[ind][prev+1];
-        int notTake=0+f(ind+1,prev,n,nums,dp);
-        int take=0;
-        if(prev==-1 || checkPossible(nums[ind],nums[prev])){
-            take=1+f(ind+1,ind,n,nums,dp);
-        }
-        return dp[ind][prev+1]=max(take,notTake);
-    }
     bool comp(string &s1,string &s2){
         return s1.size()<s2.size();
     }
@@ -31,7 +19,18 @@ public:
         sort(words.begin(), words.end(), [](string &s1, string &s2) {
             return s1.size() < s2.size();
         });
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return f(0,-1,n,words,dp);
+        vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
+        if(n==0) return 0;
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev=n-1;prev>=-1;prev--){
+                int notTake=0+dp[ind+1][prev+1];
+                int take=0;
+                if(prev==-1 || checkPossible(words[ind],words[prev])){
+                    take=1+dp[ind+1][ind+1];
+                }
+                dp[ind][prev+1]=max(take,notTake);
+            }
+        }
+        return dp[0][0];
     }
 };
