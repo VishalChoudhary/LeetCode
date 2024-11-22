2b1ba27a51fc7dce76aec343d022aec9f0b73f87
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-11-22 15:40:55 +0530

    Time: 1519 ms (39.56%), Space: 8 MB (88.74%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -9,22 +9,20 @@ public:
         }
         return true;
     }
-    int f(int i,int n,string &str,vector<int> &dp){
-        if(i==n) return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        int minCost=1e9;
-        for(int j=i;j<n;j++){
-            if(isPalindrome(i,j,str)){
-                int cost=1+f(j+1,n,str,dp);
-                minCost=min(cost,minCost);
-            }
-        }
-        return dp[i]=minCost;
-    }
     int minCut(string s) {
         int n=s.size();
-        vector<int> dp(n,-1);
-        return f(0,n,s,dp)-1;
+        vector<int> dp(n+1,0);
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            int minCost=1e9;
+            for(int j=i;j<n;j++){
+                if(isPalindrome(i,j,s)){
+                    int cost=1+dp[j+1];
+                    minCost=min(cost,minCost);
+                }
+            }
+            dp[i]=minCost;
+        }
+        return dp[0]-1;
     }
 };
