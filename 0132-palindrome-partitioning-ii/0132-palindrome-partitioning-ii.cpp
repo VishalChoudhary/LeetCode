d5d472e5ad55be1d3f77c87e7a7ac647d742d094
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-11-22 15:34:31 +0530

    Time: 1771 ms (12.56%), Space: 8.2 MB (68.75%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    // Helper function to check if a substring is a palindrome
-    bool isPalindrome(int i, int j, string &s) {
-        while (i < j) {
-            if (s[i] != s[j])
+    bool isPalindrome(int i,int j,string &s){
+        while(i<j){
+            if(s[i]!=s[j])
                 return false;
             i++;
             j--;
         }
         return true;
     }
-    
-    // Recursive function with memoization
-    int f(int i, int n, string &str, vector<int> &dp) {
-        if (i == n) return 0; // Base case: no cuts needed
-        
-        if (dp[i] != -1) // Return cached result
+    int f(int i,int n,string &str,vector<int> &dp){
+        if(i==n) return 0;
+        if(dp[i]!=-1)
             return dp[i];
-        
-        int minCost = 1e9;
-        for (int j = i; j < n; j++) {
-            if (isPalindrome(i, j, str)) {
-                int cost = 1 + f(j + 1, n, str, dp);
-                minCost = min(cost, minCost);
+        int minCost=1e9;
+        for(int j=i;j<n;j++){
+            if(isPalindrome(i,j,str)){
+                int cost=1+f(j+1,n,str,dp);
+                minCost=min(cost,minCost);
             }
         }
-        return dp[i] = minCost;
+        return dp[i]=minCost;
     }
-    
     int minCut(string s) {
-        int n = s.size();
-        vector<int> dp(n, -1);
-        return f(0, n, s, dp) - 1; // Subtract 1 to get the minimum cuts
+        int n=s.size();
+        vector<int> dp(n,-1);
+        return f(0,n,s,dp)-1;
     }
 };
