c5486d54122c3755ff8f803eac8b64a2ecc5efed
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-11-30 17:01:11 +0530

    Time: 12 ms (12.11%), Space: 5.9 MB (100%) - LeetHub

---
+++ 1058-undirected-graph-cycle-detection-using-dfs/1058-undirected-graph-cycle-detection-using-dfs.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool dfsDetect(int node,int parent,vector<vector<int>>& adj,vector<int> &vis){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                if(dfsDetect(it,node,adj,vis)==true)
+                    return true;
+            }
+            else if(parent != it)
+                return true;
+        }
+        return false;
+    }
+    bool isCycle(vector<vector<int>>& adj) {
+        // Code here
+        int V=adj.size();
+        vector<int> vis(V,0);
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                if(dfsDetect(i,-1,adj,vis)==true)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
