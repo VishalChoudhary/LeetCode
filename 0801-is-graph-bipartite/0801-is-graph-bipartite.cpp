5e8f4e8d084d56de87ed6ab4de4ca7f29343b2d2
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-12-08 13:58:07 +0530

    Time: 0 ms (100%), Space: 17.2 MB (24.1%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-    //BFS
-    bool bfs(int start,int V, vector<int> &col, vector<vector<int>>& graph){
-        queue<int> q;
-        q.push(start);
-        col[start]=0;
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            for(auto it:graph[node]){
-                //if the adj node is not yet colored
-                //give the opp color of the node
-                if(col[it]==-1){
-                    col[it] = !col[node];
-                    q.push(it);
-                }
-                //if the adj node has same color
-                //some node already colored it on diff path
-                else if(col[it]==col[node]){
+    //dfs
+    bool dfs(int node,int col, vector<int> &color, vector<vector<int>>& graph){
+        color[node]=col;
+        for(auto it:graph[node]){
+            if(color[it]==-1){
+                if(dfs(it,!col,color,graph)==false)
                     return false;
-                }
+            }
+            else if(color[it]==color[node]){
+                return false;
             }
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int V=graph.size();
-        vector<int> col(V,-1);
+        vector<int> color(V,-1);
         for(int i=0;i<V;i++){
-            if(col[i]==-1){
-                if(bfs(i,V,col,graph)==false)
+            if(color[i]==-1){
+                if(dfs(i,0,color,graph)==false)
                     return false;
             }
         }
