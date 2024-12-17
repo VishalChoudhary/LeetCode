de677db66333c7c5828fde89f16c40b299ad20c8
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-12-17 21:34:16 +0530

    Time: 15 ms (34.02%), Space: 17 MB (37.25%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    //SO
+    int f(int ind,int buy,int cap,vector<int> &prices,int n, vector<vector<vector<int>>> &dp){
+        if(cap==0) return 0;
+        if(ind==n) return 0;
+        int profit=0;
+        if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
+        if(buy){
+            profit=max(-prices[ind]+f(ind+1,0,cap,prices,n,dp),0+f(ind+1,1,cap,prices,n,dp));
+        }
+        else{
+            profit=max(prices[ind]+f(ind+1,1,cap-1,prices,n,dp),0+f(ind+1,0,cap,prices,n,dp));
+        }
+        return dp[ind][buy][cap]=profit;
+    }
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> ahead(2,vector<int>(k+1,0));
-        vector<vector<int>> curr(2,vector<int>(k+1,0));
-        //no need to include base case as it outputs 0 and in dp we declared default val as 0
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0; buy<=1; buy++){
-                //reason for cap from 1 bcz cap=0 is base case which is already covered
-                for(int cap=1;cap<=k;cap++){ 
-                    if(buy){
-                        curr[buy][cap]=max(-prices[ind]+ahead[0][cap],0+ahead[1][cap]);
-                    }
-                    else{
-                        curr[buy][cap]=max(prices[ind]+ahead[1][cap-1],0+ahead[0][cap]);
-                    }   
-                }  
-            }
-            ahead=curr;
-        }
-        return ahead[1][k];
+        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int>(k+1,-1)));
+        return f(0,1,k,prices,n,dp);
     }
 };
