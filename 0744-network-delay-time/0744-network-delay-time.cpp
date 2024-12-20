98fe65fc3efcc431569cc44ce5e16ddd6e693065
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2024-12-20 21:25:33 +0530

    Time: 993 ms (5.04%), Space: 279.5 MB (5.01%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -3,7 +3,7 @@ public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<int> dis(n+1,1e9);
         dis[k]=0;
-        for(int i=0;i<n;i++){
+        for(int i=1;i<=n;i++){
             for(auto it:times){
                 int u=it[0];
                 int v=it[1];
