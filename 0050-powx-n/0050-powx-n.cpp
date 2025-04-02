440a8dca25006350d2650d6220fd06acffde830c
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-04-03 00:54:23 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.79%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,12 +1,15 @@
 class Solution {
- public:
-  double myPow(double x, long n) {
-    if(n==0)
-        return 1;
-    if(n<1)
-        return myPow(1/x,-n);
-    if(n%2==0)
-        return myPow(x*x,n/2);
-    return x*myPow(x*x,(n-1)/2);
-  }
+public:
+    double myPow(double x, int n) {
+        long long N=n;
+        if(N==0)
+            return 1;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        if(N%2==0)
+            return myPow(x*x,N/2);
+        return x*myPow(x*x,(N-1)/2);
+    }
 };
