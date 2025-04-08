29774daf21bdddbb099c546ae09d434ca33d0176
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-04-08 18:52:43 +0530

    Time: 476 ms (64.34%), Space: 243.5 MB (32.67%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,53 +1,52 @@
 class Solution {
-    void merge(vector<int>& nums, int low, int mid, int high) {
-        vector<int> temp(high-low+1,0);
-        int ind=0;
-        int left = low;
-        int right = mid + 1;
-        while (left <= mid && right <= high) {
-            if (nums[left] <= nums[right]) {
-                temp[ind++]=nums[left++];
-            } else {
-                temp[ind++]=nums[right++];
+public:
+    void merge(vector<int> &arr, int low, int mid, int high){
+        vector<int> temp;
+        int left=low;
+        int right=mid+1;
+        while(left<=mid && right<=high){
+            if(arr[left]<=arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else{
+                temp.push_back(arr[right]);
+                right++;
             }
         }
-        while (left <= mid) {
-            temp[ind++]=nums[left++];
+        while(left<=mid){
+            temp.push_back(arr[left]);
+            left++;
         }
-        while (right <= high) {
-            temp[ind++]=nums[right++];
-        }
-        ind=0;
-        for (int i = low; i <= high; i++) {
-            nums[i] = temp[ind++];
-        }
-    }
-
- int countPairs(vector<int> &nums, int low, int mid, int high) {
-    int right = mid + 1;
-    int cnt = 0;
-    for (int i = low; i <= mid; i++) {
-        while (right <= high && static_cast<long long>(nums[i]) > 2 * static_cast<long long>(nums[right])) {
+        while(right<=high){
+            temp.push_back(arr[right]);
             right++;
         }
-        cnt += right - (mid + 1);
+        for(int i=low;i<=high;i++){
+            arr[i]=temp[i-low];
+        }
     }
-    return cnt;
-}
-
-
-    int mergeSort(vector<int>& nums, int low, int high) {
+    int countPairs(vector<int> &arr, int low, int mid, int high){
+        int right=mid+1;
         int cnt=0;
-        if (low < high) {
-            int mid = (low + high) / 2;
-            cnt+=mergeSort(nums, low, mid);
-            cnt+=mergeSort(nums, mid + 1, high);
-            cnt+=countPairs(nums,low,mid,high);
-            merge(nums, low, mid, high);
+        for(int i=low;i<=mid;i++){
+            while (right <= high && (long long)arr[i] > 2LL * arr[right])
+            right++;
+            cnt+=right-(mid+1);
         }
         return cnt;
     }
-public:
+    int mergeSort(vector<int> &arr, int low, int high){
+        int cnt=0;
+        if(low>=high)
+            return cnt;
+        int mid=(low+high)/2;
+        cnt+=mergeSort(arr,low,mid);
+        cnt+=mergeSort(arr,mid+1,high);
+        cnt+=countPairs(arr,low,mid,high);
+        merge(arr,low,mid,high);
+        return cnt;
+    }
     int reversePairs(vector<int>& nums) {
         int n=nums.size();
         return mergeSort(nums,0,n-1);
