95e3d9017f73409bf2c1f4bc73c8c453adaa3641
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-04-15 23:22:19 +0530

    Time: 0 ms (100%), Space: 16.5 MB (31.38%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,18 +1,8 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
-    ListNode *findNNode(ListNode *temp,int k){
+    ListNode *findKthNode(ListNode *temp,int i){
         int cnt=1;
         while(temp!=NULL){
-            if(cnt==k) return temp;
+            if(cnt==i) return temp;
             cnt++;
             temp=temp->next;
         }
@@ -20,7 +10,8 @@ class Solution {
     }
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL || k==0) return head;
+        if(head==NULL || k==0)
+        return head;
         ListNode *tail=head;
         int len=1;
         while(tail->next!=NULL){
@@ -30,7 +21,7 @@ public:
         if(k%len==0) return head;
         k=k%len;
         tail->next=head;
-        ListNode *newLastNode=findNNode(head,len-k);
+        ListNode *newLastNode=findKthNode(head,len-k);
         head=newLastNode->next;
         newLastNode->next=NULL;
         return head;
