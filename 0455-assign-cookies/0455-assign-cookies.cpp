8558796c0f1df925fc539b9d581878e31df5cc48
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-04-19 00:37:06 +0530

    Time: 9 ms (53.6%), Space: 44.8 MB (74.49%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -5,13 +5,13 @@ public:
         sort(s.begin(),s.end());
         int n=g.size();
         int m=s.size();
-        int child=0, cookie=0;
+        int cookie=0,child=0;
         while(child<n && cookie<m){
             if(s[cookie]>=g[child]){
                 child++;
             }
             cookie++;
-        }
+        }   
         return child;
     }
 };
