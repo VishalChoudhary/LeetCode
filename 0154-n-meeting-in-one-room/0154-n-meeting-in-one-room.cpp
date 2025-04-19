f4c8ab4da47e8ddd27f5f8ffeb5183d1320409d9
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-04-20 03:58:07 +0530

    Time: 2 ms (95.37%), Space: 11.2 MB (70.12%) - LeetHub

--- 0154-n-meeting-in-one-room/0154-n-meeting-in-one-room.cpp
+++ 0154-n-meeting-in-one-room/0154-n-meeting-in-one-room.cpp
@@ -1,25 +1,25 @@
 class Solution {
+  public:
     struct meeting{
-        int s;
-        int e;
-        int pos;
+      int s;
+      int e;
+      int pos;
     };
-public:
-    bool static comparator(struct meeting m1,meeting m2){
-        if(m1.e<m2.e) return true;
-        return false;
+    static bool comparator(meeting m1, meeting m2){
+        return m1.e<m2.e;
     }
-    // Function to find the maximum number of meetings that can
-    // be performed in a meeting room.
-    int maxMeetings(int n, int start[], int end[]) {
+    int maxMeetings(vector<int>& start, vector<int>& end) {
         // Your code here
-        struct meeting meet[n];
+        int n=start.size();
+        vector<meeting> meet(n);
         for(int i=0;i<n;i++){
-            meet[i].s=start[i],meet[i].e=end[i],meet[i].pos=i+1;
+            meet[i].s=start[i];
+            meet[i].e=end[i];
+            meet[i].pos=i+1;
         }
-        sort(meet,meet+n,comparator);
-        int freetime=meet[0].e;
+        sort(meet.begin(),meet.end(),comparator);
         int cnt=1;
+        int freetime=meet[0].e;
         for(int i=1;i<n;i++){
             if(meet[i].s>freetime){
                 cnt++;
