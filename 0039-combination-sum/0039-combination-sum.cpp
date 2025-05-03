1259d01960e229430e5983a53ce060f6174d7553
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-05-03 15:50:00 +0530

    Time: 0 ms (100%), Space: 14.1 MB (73.63%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    void findCombination(int ind,int target,vector<int> &nums,vector<int> &ds,vector<vector<int>> &ans){
-        if(ind==nums.size()){
-            if(target==0){
+    void findCombination(int ind,int target,vector<int>& arr,vector<int>& ds,
+    vector<vector<int>> &ans){
+        if(ind==arr.size()){
+            if(target == 0){
                 ans.push_back(ds);
             }
             return;
         }
-        if(nums[ind]<=target){
-            ds.push_back(nums[ind]);
-            findCombination(ind,target-nums[ind],nums,ds,ans);
+        //pickup
+        if(target>=arr[ind]){
+            ds.push_back(arr[ind]);
+            findCombination(ind,target-arr[ind],arr,ds,ans);
             ds.pop_back();
         }
-        findCombination(ind+1,target,nums,ds,ans);
+        //not-pick
+        findCombination(ind+1,target,arr,ds,ans);
     }
-
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> ds;
