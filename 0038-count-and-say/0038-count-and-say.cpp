13eeb682b66832fb965f0d65167c0a44e2172ade
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-05-11 13:33:41 +0530

    Time: 6 ms (27.77%), Space: 9.8 MB (79.82%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -3,16 +3,16 @@ public:
     string countAndSay(int n) {
         if(n==1)
         return "1";
-        string say=countAndSay(n-1);
         string res="";
+        string say=countAndSay(n-1);
         for(int i=0;i<say.size();i++){
             char ch=say[i];
-            int count=1;
-            while(i<say.length()-1 && say[i]==say[i+1]){
-                count++;
+            int cnt=1;
+            while(i<say.size()-1 && say[i]==say[i+1]){
+                cnt++;
                 i++;
             }
-            res+=to_string(count)+string(1,ch);
+            res+=to_string(cnt)+string(1,ch);
         }
         return res;
     }
