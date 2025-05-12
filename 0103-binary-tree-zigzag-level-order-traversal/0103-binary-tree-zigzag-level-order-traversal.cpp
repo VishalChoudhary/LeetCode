e17a5ef15f1cba41d15cf921cf71c0564fd6d41e
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-05-13 02:50:20 +0530

    Time: 0 ms (100%), Space: 14.9 MB (93.91%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,35 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> ans;
         if(root==NULL)
         return ans;
-        queue<TreeNode*> q;
+        queue<TreeNode *> q;
         q.push(root);
-        bool leftToRight=true;
+        bool ltr=true;
         while(!q.empty()){
             int size=q.size();
-            vector<int> ds(size);
+            vector<int>ds(size);
             for(int i=0;i<size;i++){
                 TreeNode *node=q.front();
                 q.pop();
-                int index=(leftToRight)?i:(size-i-1);
+                if(node->left!=NULL) q.push(node->left);
+                if(node->right!=NULL) q.push(node->right);
+                int index=ltr?i:size-1-i;
                 ds[index]=node->val;
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
             }
-            leftToRight=!leftToRight;
+            ltr=!ltr;
             ans.push_back(ds);
         }
         return ans;
