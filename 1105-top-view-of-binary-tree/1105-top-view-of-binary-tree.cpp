957840a47349f9dcb87e67124845f1ceed717d42
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-05-19 20:01:46 +0530

    Time: 10 ms (38.1%), Space: 21.4 MB (46.46%) - LeetHub

--- 1105-top-view-of-binary-tree/1105-top-view-of-binary-tree.cpp
+++ 1105-top-view-of-binary-tree/1105-top-view-of-binary-tree.cpp
@@ -1,25 +1,26 @@
 class Solution {
   public:
-    vector <int> bottomView(Node *root) {
-        // Your Code Here
-        vector<int> ans;
-        if(root==NULL)
-        return ans;
-        map<int,int>mpp;
-        queue<pair<Node*,int>> q;
-        q.push({root,0});
-        while(!q.empty()){
-            auto it=q.front();
-            q.pop();
-            Node *node=it.first;
-            int line=it.second;
-            mpp[line]=node->data;
-            if(node->left) q.push({node->left,line-1});
-            if(node->right) q.push({node->right,line+1});
+    vector<int> getTopView(TreeNode<int> *root){
+    vector<int> ans;
+    if(root==NULL)
+    return ans;
+    map<int,int> mpp;
+    queue<pair<TreeNode<int>*, int>> q;
+    q.push({root,0});
+    while(!q.empty()){
+        auto it = q.front();
+        q.pop();
+        TreeNode<int> *node = it.first;
+        int line = it.second;
+        if(mpp.find(line)==mpp.end()){
+            mpp[line] = node->data;
         }
-        for(auto it:mpp){
-            ans.push_back(it.second);
-        }
-        return ans;
+        if(node->left) q.push({node->left,line-1});
+        if(node->right) q.push({node->right,line+1});
     }
+    for(auto x:mpp){
+        ans.push_back(x.second);
+    }
+    return ans;
+  }
 };
