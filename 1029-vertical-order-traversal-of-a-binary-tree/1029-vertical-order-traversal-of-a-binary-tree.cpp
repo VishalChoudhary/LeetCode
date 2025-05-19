4b320abe713f32778e85f3c8ffeed9c1501cd71b
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-05-19 17:09:27 +0530

    Time: 2 ms (51.01%), Space: 16.4 MB (43.65%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        queue<pair<TreeNode*,pair<int,int>>> q;
         map<int,map<int,multiset<int>>> ds;
+        queue<pair<TreeNode*,pair<int,int>>> q;
         q.push({root,{0,0}});
         while(!q.empty()){
             auto p=q.front();
             q.pop();
-            TreeNode *node=p.first;
-            int x=p.second.first;
-            int y=p.second.second;
+            TreeNode *node = p.first;
+            int x = p.second.first;
+            int y = p.second.second;
             ds[x][y].insert(node->val);
             if(node->left) q.push({node->left,{x-1,y+1}});
             if(node->right) q.push({node->right,{x+1,y+1}});
@@ -17,8 +17,8 @@ public:
         vector<vector<int>> ans;
         for(auto p:ds){
             vector<int> col;
-            for(auto t:p.second){
-                col.insert(col.end(),t.second.begin(),t.second.end());
+            for(auto q:p.second){
+                col.insert(col.end(),q.second.begin(),q.second.end());
             }
             ans.push_back(col);
         }
