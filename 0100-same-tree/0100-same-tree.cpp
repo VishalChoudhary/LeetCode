25ca17aece7c54e893bd307b8abbd81b547682dd
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-05-19 20:33:57 +0530

    Time: 0 ms (100%), Space: 12.8 MB (77.82%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,8 +14,8 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL || q==NULL)
         return p==q;
-        return (p->val==q->val)
-        && isSameTree(p->left,q->left)
-        && isSameTree(p->right,q->right);
+        return (p->val == q->val) &&
+            isSameTree(p->left,q->left) &&
+            isSameTree(p->right,q->right);
     }
 };
