679133ee56301c0d6e9a7d29421a8521e3415264
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-05-21 08:30:25 +0530

    Time: 0 ms (100%), Space: 19.6 MB (68.19%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
@@ -19,16 +8,16 @@ public:
         q.push({root,0});
         while(!q.empty()){
             int size=q.size();
-            long long int minm=q.front().second;
+            long long int minm = q.front().second;
             long long int first,last;
             for(int i=0;i<size;i++){
-                long long int curr_id=q.front().second-minm;
-                TreeNode *node=q.front().first;
+                long long int currId = q.front().second-minm;
+                TreeNode * node = q.front().first;
                 q.pop();
-                if(i==0) first=curr_id;
-                if(i==size-1) last=curr_id;
-                if(node->left) q.push({node->left,curr_id*2+1});
-                if(node->right) q.push({node->right,curr_id*2+2});
+                if(i==0) first = currId;
+                if(i==size-1) last = currId;
+                if(node->left) q.push({node->left,currId*2+1});
+                if(node->right) q.push({node->right,currId*2+2});
             }
             ans=max(ans,static_cast<int>(last-first+1));
         }
