0e4e2fe7380868e1e4ee4b368251d76bba932976
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-06-09 13:37:23 +0530

    Time: 0 ms (100%), Space: 12.8 MB (59.02%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -6,20 +6,16 @@ public:
         unordered_map<int,int> mpp;
         int n=nums2.size();
         for(int i=n-1;i>=0;i--){
-            while(!st.empty() && st.top()<=nums2[i]){
+            while(!st.empty() && st.top()<=nums2[i])
                 st.pop();
-            }
-            if(!st.empty()){
-                mpp[nums2[i]]=st.top();
-            }
-            else{
+            if(st.empty())
                 mpp[nums2[i]]=-1;
-            }
+            else
+                mpp[nums2[i]]=st.top();
             st.push(nums2[i]);
         }
-        for(int i=0;i<nums1.size();i++){
-            ans.push_back(mpp[nums1[i]]);
-        }
+        for(int i=0;i<nums1.size();i++)
+        ans.push_back(mpp[nums1[i]]);
         return ans;
     }
 };
