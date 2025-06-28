5acbf4d6963160d0eb4a75db055dfd53300aea67
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-06-28 16:02:38 +0530

    Time: 0 ms (100%), Space: 8.9 MB (33.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,11 +4,11 @@ public:
         stack<char> st;
         for(auto i:s){
             if(i=='(' || i=='{' || i=='[')
-                st.push(i);
+            st.push(i);
             else{
                 if(st.empty())
-                    return false;
-                char top = st.top();
+                return false;
+                char top=st.top();
                 if(i==')' && top!='(' ||
                    i=='}' && top!='{' ||
                    i==']' && top!='['
