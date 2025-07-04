0a5b764019e175c95db0574b40f30c04e8c3069f
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-07-04 16:01:53 +0530

    Time: 3 ms (97.67%), Space: 9.4 MB (82.5%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -3,11 +3,12 @@ public:
     string longestPalindrome(string s) {
         if(s.length()<=1)
         return s;
-        int max_len=1;
         int n=s.length();
+        int max_len=1;
         int st=0,end=0;
+
         //odd
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;++i){
             int l=i,r=i;
             while(l>=0 && r<n){
                 if(s[l]==s[r]){
@@ -16,14 +17,15 @@ public:
                 else break;
             }
             int len=r-l-1;
-            if(len>max_len) {
+            if(len>max_len){
                 max_len=len;
                 st=l+1;
                 end=r-1;
             }
         }
+
         //even
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;++i){
             int l=i,r=i+1;
             while(l>=0 && r<n){
                 if(s[l]==s[r]){
@@ -32,7 +34,7 @@ public:
                 else break;
             }
             int len=r-l-1;
-            if(len>max_len) {
+            if(len>max_len){
                 max_len=len;
                 st=l+1;
                 end=r-1;
