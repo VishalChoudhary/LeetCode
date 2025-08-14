5608931b24c714c31bb136a7b0fb3419052606df
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-08-14 18:30:37 +0530

    Time: 223 ms (22.9%), Space: 126.4 MB (41.39%) - LeetHub

--- 2656-count-the-number-of-beautiful-subarrays/2656-count-the-number-of-beautiful-subarrays.cpp
+++ 2656-count-the-number-of-beautiful-subarrays/2656-count-the-number-of-beautiful-subarrays.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     long long beautifulSubarrays(vector<int>& nums) {
-        long long xr=0;
+        int xr=0;
         map<int,int> mpp;
-        long long cnt=0;
         mpp[xr]++;
-        for(int i=0;i<nums.size();i++){
+        long long cnt=0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
             xr=xr^nums[i];
-            int x=xr^0;
+            int x = xr^0;
             cnt+=mpp[x];
             mpp[xr]++;
-        } 
+        }
         return cnt;
     }
 };
