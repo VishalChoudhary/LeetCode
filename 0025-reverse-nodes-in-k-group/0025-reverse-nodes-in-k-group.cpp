817901a50988d63852505bf8203d5fca94dd3c93
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-08-18 13:09:42 +0530

    Time: 0 ms (100%), Space: 16.6 MB (39.18%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,16 +1,17 @@
 class Solution {
-    ListNode *reverseLL(ListNode *head){
-        ListNode *nextnode=head, *curr=head, *prev=NULL;
-        while(nextnode!=NULL){
-            nextnode=nextnode->next;
+public:
+    ListNode* reverseLL(ListNode *head){
+        ListNode *nextNode=head, *curr=head,*prev=NULL;
+        while(nextNode!=NULL){
+            nextNode=nextNode->next;
             curr->next=prev;
             prev=curr;
-            curr=nextnode;
-        }
+            curr=nextNode;
+        }        
         head=prev;
-        return prev;
+        return head;
     }
-    ListNode *getKthNode(ListNode *temp,int k){
+    ListNode* getKthNode(ListNode *temp,int k){
         k-=1;
         while(temp!=NULL && k>0){
             k--;
@@ -18,25 +19,26 @@ class Solution {
         }
         return temp;
     }
-public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode *temp=head;
         ListNode *prev=NULL;
         while(temp!=NULL){
-            ListNode *kthNode=getKthNode(temp,k);
+            ListNode *kthNode = getKthNode(temp,k);
             if(kthNode==NULL){
                 if(prev) prev->next=temp;
                 break;
-            }   
-            ListNode *nextnode=kthNode->next;
+            }
+            ListNode *nextNode = kthNode->next;
             kthNode->next=NULL;
             reverseLL(temp);
-            if(temp==head)
-                head=kthNode;  
-            else
+            if(temp==head){
+                head=kthNode;
+            }
+            else{
                 prev->next=kthNode;
+            }   
             prev=temp;
-            temp=nextnode;     
+            temp=nextNode;
         }
         return head;
     }
