6523e7ae75f60415a0dc2bb542dc0c649b31c0a1
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-08-20 09:36:10 +0530

    Time: 3 ms (94.99%), Space: 44.9 MB (43.54%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,15 +3,14 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        int n=g.size();
-        int m=s.size();
-        int cookie=0,child=0;
+        int n = g.size(), m = s.size();
+        int child=0, cookie=0;
         while(child<n && cookie<m){
             if(s[cookie]>=g[child]){
                 child++;
             }
             cookie++;
-        }   
+        }
         return child;
     }
 };
