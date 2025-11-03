ed6eb335dd88e4c5b3d0d4c3c72eecca57f57cb5
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-11-03 17:17:12 +0530

    Time: 0 ms (100%), Space: 13.9 MB (97.93%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    void findCombination(int ind,int target,vector<int>& nums,vector<int> &ds,vector<vector<int>> &ans){
+    void findC(int ind,int target,vector<int> &nums,vector<int> &ds,vector<vector<int>> &ans){
         if(target==0){
             ans.push_back(ds);
             return;
         }
         for(int i=ind;i<nums.size();i++){
-            if(i!=ind && nums[i]==nums[i-1])
-            continue;
+            if(i!=ind && nums[i]==nums[i-1]) continue;
             if(nums[i]>target) break;
             ds.push_back(nums[i]);
-            findCombination(i+1,target-nums[i],nums,ds,ans);
+            findC(i+1,target-nums[i],nums,ds,ans);
             ds.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
         vector<vector<int>> ans;
         vector<int> ds;
-        findCombination(0,target,candidates,ds,ans);
+        sort(candidates.begin(),candidates.end());
+        findC(0,target,candidates,ds,ans);
         return ans;
     }
 };
