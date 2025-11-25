2013a1711294ca2fe80611570add06b0cd8dd0e1
Vishal Choudhary <54886683+VishalChoudhary@users.noreply.github.com>
2025-11-25 13:06:44 +0530

    Time: 0 ms (100%), Space: 28.3 MB (28.25%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,27 @@
-//using map
+//using Moore's voting algorithm
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        map<int,int> mpp;
+        int cnt=0, n=nums.size();
+        int el=0;
         for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it:mpp){
-            if(it.second>n/2){
-                return it.first;
+            if(cnt==0){
+                cnt+=1;
+                el=nums[i];
             }
+            else if(el==nums[i]){
+                cnt+=1;
+            }
+            else cnt-=1;
+        }
+        int cnt1=0;
+        for(int i=0;i<n;i++){
+            if(el==nums[i]){
+                cnt1+=1;
+            }
+        }
+        if(cnt1>n/2){
+            return el;
         }
         return -1;
     }
